807e72674840b9492495b11100118ff73ec03012
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-12-24 11:17:31 +0300

    Add old() data to create new threads.

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -7,33 +7,53 @@
                 <div class="card">
                     <div class="card-header">Create New Thread</div>
 
+                    @if (count($errors))
+                        <ul class="alert alert-danger" role="alert">
+                            @foreach($errors->all() as $error)
+                                <li>{{$error}}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+
                     <div class="card-body">
                         <form action="/threads" method="POST">
 
                             {{csrf_field()}}
-                            {{--@csrf--}}
 
                             <div class="form-group">
                                 <label for="title">Title:</label>
-                                <input type="text" class="form-control" name="title" id="title">
+                                <input type="text" class="form-control" name="title" id="title"
+                                       value="{{old('title')}}" required>
+                                @if($errors->has('title'))
+                                    <div class="alert alert-danger">{{$errors->first('title')}}</div>
+                                @endif
                             </div>
 
                             <div class="form-group">
                                 <label for="body">Body:</label>
-                                <textarea class="form-control" name="body" id="body" rows="8"></textarea>
+                                <textarea class="form-control" required name="body" id="body" rows="8">{{old('body')}}</textarea>
+                                @if($errors->has('body'))
+                                    <div class="alert alert-danger">{{$errors->first('body')}}</div>
+                                @endif
                             </div>
 
                             <div class="form-group">
-                                <select name="channel_id" id="">
+                                <select name="channel_id" id="" class="form-control" required>
+                                    <option value="">Choose channel...</option>
                                     @foreach($channels as $channel)
-                                        <option value='{{ $channel->id }}'>{{ $channel->name }}</option>
+                                        <option value='{{ $channel->id }}'
+                                        {{old('channel_id') == $channel->id ? 'selected' : ''}}
+                                        >{{ $channel->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
 
-                            <button type="submit" class="btn btn-primary">Publish</button>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Publish</button>
+                            </div>
 
                         </form>
+
 
                     </div>
                 </div>
